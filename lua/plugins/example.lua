dd0d74d4a846ee97f05546443c29bb67afd7e19f
Vasylkivskyi <pavlo.vasylkivskyi@gmail.com>
2024-10-13 20:00:40 +0200

    feat: ts import

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -209,4 +209,60 @@ return {
       ]])
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- LSP completions
+      "hrsh7th/cmp-buffer", -- Buffer completions
+      "hrsh7th/cmp-path", -- Path completions
+      "hrsh7th/cmp-cmdline", -- Cmdline completions
+      "saadparwaiz1/cmp_luasnip", -- Snippet completions
+      "L3MON4D3/LuaSnip", -- Snippet engine
+      "rafamadriz/friendly-snippets", -- Snippet collection
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" }, -- LSP source
+          { name = "luasnip" }, -- Snippet source
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+
+  -- Setup LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim", -- TypeScript support
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Setup tsserver for TypeScript/JavaScript
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          -- Configure keymaps for the LSP
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        end,
+      })
+    end,
+  },
 }
